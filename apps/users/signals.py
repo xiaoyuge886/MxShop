48cc73f143e915191fe46238476d549779e5cd25
gy <yu.guo@capitalonline.net>
2019-07-24 08:00:02 +0800

    xadmin 登录失败注释users signals代码

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -8,14 +8,14 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-# post_save:接收信号的方式
-#sender: 接收信号的model
-@receiver(post_save, sender=User)
-def create_user(sender, instance=None, created=False, **kwargs):
-    # 是否新建，因为update的时候也会进行post_save
-    if created:
-        password = instance.password
-        #instance相当于user
-        instance.set_password(password)
-        instance.save()
-
+# # post_save:接收信号的方式
+# #sender: 接收信号的model
+# @receiver(post_save, sender=User)
+# def create_user(sender, instance=None, created=False, **kwargs):
+#     # 是否新建，因为update的时候也会进行post_save
+#     if created:
+#         password = instance.password
+#         #instance相当于user
+#         instance.set_password(password)
+#         instance.save()
+#
